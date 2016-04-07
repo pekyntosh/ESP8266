0abb301777cd72adfb350c188ebcde6bbd58be15
Pekyntosh <stefanopardini@gmail.com>
2016-04-08 00:07:22 +0200

    Some small fixes

--- temp/temp.ino
+++ temp/temp.ino
@@ -13,7 +13,8 @@ https://datasheets.maximintegrated.com/en/ds/DS18B20.pdf
 const byte PIN_20 = 2;
 
 const char* AP_SSID = "your wifi ap";
-const char* AP_PASSWORD = "your ap password";
+const char* AP_PASSWORD = "your wifi password";
+const char* HOSTNAME = "ESP8266";
 
 OneWire oneWire(PIN_20);
 DallasTemperature DS18B20(&oneWire);
@@ -38,17 +39,18 @@ void loop() {
 }
 
 void web_server_init() {	
-	server.on("/", handle_index()); 
+	server.on("/", handle_index); 
 	
 /*	
+ * reminder to handle callback
 	server.on("/temp", []() {
 		float temp = get_temp(); // live sensor reader
 		String to_send = "Temperature: " + String((int) temp) + "° C";
 		server.send(200, "text/plain", to_send);
 	});
 */
-	server.on("/temp", handle_temp());
-	server.on("/status", handle_status());
+	server.on("/temp", handle_temp);
+	server.on("/status", handle_status);
 	
 	server.begin();
 	Serial.println("HTTP server started");	
@@ -56,7 +58,13 @@ void web_server_init() {
 
 void wifi_connect() {
 	Serial.println("Connecting to AP " + (String) AP_SSID);
-	WiFi.begin(AP_SSID, AP_PASSWORD);
+
+  WiFi.hostname(HOSTNAME);
+// starts wifi in DHCP mode (i've reserved a DHCP lease, associated with the mac address of the device)
+  WiFi.begin(AP_SSID, AP_PASSWORD);
+// or you can manually set your layer3 info
+//  WiFi.config(ip, dns, gateway, subnet); 
+ 
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(1000);
 		Serial.print(".");
@@ -68,13 +76,21 @@ void wifi_connect() {
 }
 
 float get_temp() {
-	DS18B20.requestTemperatures();
-	float temp = DS18B20.getTempCByIndex(0);
-	
+  float temp = 0;
+  const byte readings = 5;
+  
+// 5 performs delayed temperature readings
+  for (int k = 0; k < 5; k++) {
+    DS18B20.requestTemperatures();
+// Why "byIndex"? You can have more than one IC on the same bus. 0 refers     
+    temp += DS18B20.getTempCByIndex(0);
+    delay(1);
+  }
+  temp = temp / readings;
+  
 	Serial.print("Temperature: ");
 	Serial.println(temp);
-//	while (temp == 85.0 || temp == (-127.0));
-
+ 
 	return temp;
 }
 
@@ -85,7 +101,7 @@ void handle_index() {
 
 void handle_temp() {
 	float temp = get_temp(); // on demand sensor reader
-	String to_send = "Temperature: " + String((int) temp) + " C";
+	String to_send = String((float) temp);
 	server.send(200, "text/plain", to_send);
 }
 
